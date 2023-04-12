30039a36c9c8e81941b363f0746f48465cc89d88
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-12 14:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let mapping = {
+		'0':'0',
+		'1': '1',
+		'2': 'abc',
+		'3': 'def',
+		'4': 'ghi',
+		'5': 'jkl',
+		'6': 'mno',
+		'7': 'pqrs',
+		'8': 'tuv',
+		'9': 'wxyz'
+	};
+	let ans = [];
+	combo(0,mapping,ans,input_digit,'');
+	ans.sort();
+	return ans;
+}
+function combo(ind,mapping,ans,input_digit,str) {
+	if(ind==input_digit.length) {
+		ans.push(str);
+		return;
+	}
+	for (let i = 0; i < mapping[input_digit[ind]].length; i++) {
+		let mapped =  mapping[input_digit[ind]];
+		combo(ind+1,mapping,ans,input_digit,str+mapped[i])';'
+	}
 }
 
 module.exports = letterCombinations;
